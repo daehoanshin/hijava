13a942125cb91a00cfe716add1c38586708aaa97
daehoanshin <xbb123@naver.com>
2022-07-03 15:35:53 +0900

    first commit

---
+++ src/hijava/practice/HelloJava.java
@@ -0,0 +1,17 @@
+package hijava.practice;
+
+/**
+ * 첫번쨰 자바 실습
+ * @author dhshin
+ * @update say함수 추가 (2022-06-23
+ */
+public class HelloJava {
+	String aa = "";
+	public static void main(String[] args) {
+		HelloJava.say("hi~");
+	}
+	
+	public static void say(String msg) {
+		System.out.println(msg);
+	}
+}
---
+++ src/hijava/practice/Str.java
@@ -0,0 +1,17 @@
+package hijava.practice;
+
+public class Str {
+	public static void main(String[] args) {
+		char c = '한'; // cf. char c = 65;
+		byte b = 'B';
+		System.out.println(c);
+		System.out.println((int)c);
+		System.out.println(b);
+		
+		String str = "AB";
+		System.out.println(str);
+		System.out.println("AB".getBytes().length);
+		System.out.println("AB한".getBytes().length);
+		System.out.println("한".getBytes().length);
+	}
+}
